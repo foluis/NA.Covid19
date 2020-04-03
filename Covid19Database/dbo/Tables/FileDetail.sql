55d46eafb3efc5f66c0a83088c3191a41dd02fae
Luis Forero <foluis@hotmail.com>
2020-04-03 13:50:05 -0600

    Add database

---
+++ Covid19Database/dbo/Tables/FileDetail.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[FileDetail]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Province_State] VARCHAR(50) NOT NULL, 
+    [Country_Region] VARCHAR(50) NOT NULL, 
+    [Last_Update] DATETIME2 NOT NULL, 
+    [Lat] DECIMAL(13, 7) NOT NULL, 
+    [Long] DECIMAL(13, 7) NOT NULL, 
+    [Confirmed] INT NOT NULL, 
+    [Deaths] INT NOT NULL, 
+    [Recovered] INT NOT NULL, 
+    [Active] INT NOT NULL, 
+    [FileId] INT NOT NULL,
+    CONSTRAINT[FK_FileDetail_FileId] FOREIGN KEY ([FileId])REFERENCES [FileDownloadInfo] (Id)
+);
---
+++ Covid19Database/dbo/Tables/FileDownloadInfo.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[FileDownloadInfo]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [DownloadedDate] DATETIME2 NOT NULL 
+)
